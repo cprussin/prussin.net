10489fb85ab406e059bbff6be1621b4be2049f76
Connor Prussin <connor@prussin.net>
2023-04-07 14:31:19 -0700

    Fix bad formatting

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -4,10 +4,12 @@ const nextConfig = {
     appDir: true,
     typedRoutes: true,
   },
-  rewrites: () => [{
-    source: "/.well-known/matrix/server",
-    destination: "/matrix-server.json"
-  }]
+  rewrites: () => [
+    {
+      source: "/.well-known/matrix/server",
+      destination: "/matrix-server.json",
+    },
+  ],
 };
 
 export default nextConfig;
